224bec4097c5cd183b671fd6bafd8f56fd26f468
sabelo23 <sabelomhlongo4@gmail.com>
2023-05-24 18:08:51 -0700

    changes

---
+++ linked_list.c
@@ -0,0 +1,127 @@
+#include "shell.h"
+
+/**
+ * print_list - The function that will print linked list
+ * @h: The linked list
+ * Return: size of linked list
+ */
+
+size_t print_list(list_t *h)
+{
+	list_t *c_list = h;
+	int count = 0;
+	int c = 0;
+
+	if (h == NULL)
+		return (0);
+	while (c_list != NULL)
+	{
+		if (c_list->var == NULL)
+		{
+			write(STDOUT_FILENO, "(nil)", 5);
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		else
+		{
+			c = 0;
+			while ((c_list->var)[c] != '\0')
+				c++;
+		write(STDOUT_FILENO, c_list->var, c);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	c_list = c_list->next;
+	count++;
+	}
+	return (count);
+}
+
+/**
+ * add_end_node - The function that add node to linked list
+ * @head: A pointer to head of the linked list
+ * @str: The data to new node
+ * Return: A pointer to new linked list
+ */
+
+list_t *add_end_node(list_t **head, char *str)
+{
+	list_t *new;
+	list_t *holder;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+			return (NULL);
+	holder = *head;
+	if (holder != NULL)
+	{
+		while (holder->next != NULL)
+		{
+			holder = holder->next;
+		}
+		holder->next = new;
+	}
+	else
+	{
+		*head = new;
+	}
+	return (*head);
+}
+
+/**
+ * delete_nodeint_at_index - The function that removing node at index
+ * @head: A input head address
+ * @index: A input index
+ * Return: 1 returned if successful or -1 if not successful.
+ */
+
+int delete_nodeint_at_index(list_t **head, int index)
+{
+	list_t *n_head;
+	list_t *holder;
+	int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		holder = (*head)->next;
+		free((*head)->var);
+		free(*head);
+		*head = holder;
+		return (1);
+	}
+	count = 1;
+	n_head = *head;
+	while (count < index)
+	{
+		if (n_head == NULL)
+			return (-1);
+		n_head = n_head->next;
+		count++;
+	}
+	holder = n_head->next;
+	n_head->next = holder->next;
+	free(holder->var);
+	free(holder);
+	return (1);
+}
+
+/**
+ * free_linked_list - frees linked list
+ * @list: linked list
+ */
+void free_linked_list(list_t *list)
+{
+	list_t *holder;
+
+	while (list != NULL)
+	{
+		holder = list;
+		list = list->next;
+		free(holder->var);
+		free(holder);
+	}
+
+}
+
